c90c7af97a230165091fe0bf2e045c5ba8c7fd4b
inatoihs <sabanna.taihei@gmail.com>
2021-12-07 17:55:49 +0900

    origin

--- script/camera.js
+++ script/camera.js
@@ -5,107 +5,117 @@
 	License: MIT
 */
 
-var camera = (function() {
-	var options;
-	var video, canvas, context;
-	var renderTimer;
+var camera = (function () {
+    var options;
+    var video, canvas, context;
+    var renderTimer;
 
-	function initVideoStream() {
-		video = document.createElement("video");
-		video.setAttribute('width', options.width);
-		video.setAttribute('height', options.height);
-		video.setAttribute('playsinline', 'true');
-		video.setAttribute('webkit-playsinline', 'true');
+    function initVideoStream() {
+        video = document.createElement("video");
+        video.setAttribute("width", options.width);
+        video.setAttribute("height", options.height);
+        video.setAttribute("playsinline", "true");
+        video.setAttribute("webkit-playsinline", "true");
 
-		navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia;
-		window.URL = window.URL || window.webkitURL || window.mozURL || window.msURL;
+        navigator.getUserMedia =
+            navigator.getUserMedia ||
+            navigator.webkitGetUserMedia ||
+            navigator.mozGetUserMedia ||
+            navigator.msGetUserMedia;
+        window.URL =
+            window.URL || window.webkitURL || window.mozURL || window.msURL;
 
-		if (navigator.getUserMedia) {
-			navigator.getUserMedia({
-				video: true,
-				audio: false,
-			}, function(stream) {
-				options.onSuccess();
+        if (navigator.getUserMedia) {
+            navigator.getUserMedia(
+                {
+                    video: true,
+                    audio: false,
+                },
+                function (stream) {
+                    options.onSuccess();
 
-				if (video.mozSrcObject !== undefined) { // hack for Firefox < 19
-					video.mozSrcObject = stream;
-				} else {
-					video.srcObject = stream;
-				}
+                    if (video.mozSrcObject !== undefined) {
+                        // hack for Firefox < 19
+                        video.mozSrcObject = stream;
+                    } else {
+                        video.srcObject = stream;
+                    }
 
-				initCanvas();
-			}, options.onError);
-		} else {
-			options.onNotSupported();
-		}
-	}
+                    initCanvas();
+                },
+                options.onError
+            );
+        } else {
+            options.onNotSupported();
+        }
+    }
 
-	function initCanvas() {
-		canvas = options.targetCanvas || document.createElement("canvas");
-		canvas.setAttribute('width', options.width);
-		canvas.setAttribute('height', options.height);
+    function initCanvas() {
+        canvas = options.targetCanvas || document.createElement("canvas");
+        canvas.setAttribute("width", options.width);
+        canvas.setAttribute("height", options.height);
 
-		context = canvas.getContext('2d');
+        context = canvas.getContext("2d");
 
-		// mirror video
-		if (options.mirror) {
-			context.translate(canvas.width, 0);
-			context.scale(-1, 1);
-		}
-	}
+        // mirror video
+        if (options.mirror) {
+            context.translate(canvas.width, 0);
+            context.scale(-1, 1);
+        }
+    }
 
-	function startCapture() {
-		video.play();
+    function startCapture() {
+        video.play();
 
-		renderTimer = setInterval(function() {
-			try {
-				context.drawImage(video, 0, 0, video.width, video.height);
-				options.onFrame(canvas);
-			} catch (e) {
-				// TODO
-			}
-		}, Math.round(1000 / options.fps));
-	}
+        renderTimer = setInterval(function () {
+            try {
+                context.drawImage(video, 0, 0, video.width, video.height);
+                options.onFrame(canvas);
+            } catch (e) {
+                // TODO
+            }
+        }, Math.round(1000 / options.fps));
+    }
 
-	function stopCapture() {
-		pauseCapture();
+    function stopCapture() {
+        pauseCapture();
 
-		if (video.mozSrcObject !== undefined) {
-			video.mozSrcObject = null;
-		} else {
-			video.srcObject = null;
-		}
-	}
+        if (video.mozSrcObject !== undefined) {
+            video.mozSrcObject = null;
+        } else {
+            video.srcObject = null;
+        }
+    }
 
-	function pauseCapture() {
-		if (renderTimer) clearInterval(renderTimer);
-		video.pause();
-	}
+    function pauseCapture() {
+        if (renderTimer) clearInterval(renderTimer);
+        video.pause();
+    }
 
-	return {
-		init: function(captureOptions) {
-			var doNothing = function(){};
+    return {
+        init: function (captureOptions) {
+            var doNothing = function () {};
 
-			options = captureOptions || {};
+            options = captureOptions || {};
 
-			options.fps = options.fps || 30;
-			options.width = options.width || 640;
-			options.height = options.height || 480;
-			options.mirror = options.mirror || false;
-			options.targetCanvas = options.targetCanvas || null; // TODO: is the element actually a <canvas> ?
+            options.fps = options.fps || 30;
+            options.width = options.width || 640;
+            options.height = options.height || 480;
+            options.mirror = options.mirror || false;
+            options.targetCanvas = options.targetCanvas || null; // TODO: is the element actually a <canvas> ?
 
-			options.onSuccess = options.onSuccess || doNothing;
-			options.onError = options.onError || doNothing;
-			options.onNotSupported = options.onNotSupported || doNothing;
-			options.onFrame = options.onFrame || doNothing;
+            options.onSuccess = options.onSuccess || doNothing;
+            options.onError = options.onError || doNothing;
+            options.onNotSupported = options.onNotSupported || doNothing;
+            options.onFrame = options.onFrame || doNothing;
 
-			initVideoStream();
-		},
+            initVideoStream();
+        },
 
-		start: startCapture,
+        start: startCapture,
 
-		pause: pauseCapture,
+        pause: pauseCapture,
 
-		stop: stopCapture
-	};
+        stop: stopCapture,
+    };
 })();
